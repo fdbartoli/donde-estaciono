1476584a50938e6cf3e2ab18a879292dc214ba38
Fernando Bartoli <fdbartoli91@gmail.com>
2025-10-26 09:31:17 -0300

    add component - fix deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-
-  // Login (lazy)
   { path: 'login', loadComponent: () => import('./login/login').then(m => m.LoginComponent) },
 
-  // Catch-all
-  { path: '**', redirectTo: '' }
+  {
+    path: 'buscar-estacionamiento',
+    loadComponent: () =>
+      import('./pages/event-search/event-search.component').then(m => m.EventSearchComponent),
+  },
+
+  { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/event-search/event-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-event-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './event-search.component.html',
+  styleUrls: ['./event-search.component.scss'],
+})
+export class EventSearchComponent {}
